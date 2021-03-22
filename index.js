7bddfd0e85ab5b99acadc2afa77714a069b1fb77
Luca Ziliani <lucagcc@gmail.com>
2021-03-22 12:54:56 +0100

    add swipeByPosition feature

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const sleep = require('p-sleep')
 
 const settings = {
   snapBackDuration: 300,
+  defaultAnimateOutDuration: 100,
   maxTilt: 5,
   bouncePower: 0.2,
-  swipeThreshold: 300 // px/s
+  swipeThreshold: 300, // px/s
+  swipeByPositionTreshold: 25 // body %
 }
 
 const getElementSize = (element) => {
@@ -47,27 +49,54 @@ const pythagoras = (x, y) => {
   return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
 }
 
-const animateOut = async (element, speed, easeIn = false, disableRotationOnAnimateOutAndBack = false) => {
-  const startPos = getTranslate(element)
-  const bodySize = getElementSize(document.body)
-  const diagonal = pythagoras(bodySize.x, bodySize.y)
+const animateOut = async (
+  element,
+  speed,
+  easeIn = false,
+  disableRotationOnAnimateOutAndBack = false,
+  dir = 'right'
+) => {
+  const bodySize = getElementComputedStyle(document.body)
+  let time, translateString
+  if (speed === false) {
+    // swipedByPosition
+    let tx = 0
+    let ty = 0
+    if (dir === 'right') {
+      tx = bodySize.x + bodySize.paddingRight
+    } else if (dir === 'left') {
+      tx = (bodySize.x + bodySize.paddingLeft) * -1
+    } else if (dir === 'up') {
+      ty = (bodySize.y + bodySize.paddingTop) * -1
+    } else if (dir === 'down') {
+      ty = bodySize.y + bodySize.paddingBottom
+    }
+    translateString = translationString(tx, ty)
+    time = settings.defaultAnimateOutDuration / 1000
+  } else {
+    // swipedBySpeed
+    const startPos = getTranslate(element)
+    const diagonal = pythagoras(bodySize.x, bodySize.y)
 
-  const velocity = pythagoras(speed.x, speed.y)
-  const time = diagonal / velocity
-  const multiplier = diagonal / velocity
+    const velocity = pythagoras(speed.x, speed.y)
+    time = diagonal / velocity
+    const multiplier = diagonal / velocity
 
-  const translateString = translationString(speed.x * multiplier + startPos.x, -speed.y * multiplier + startPos.y)
-  let rotateString = ''
-
-  const rotationPower = 200
-
+    translateString = translationString(
+      speed.x * multiplier + startPos.x,
+      -speed.y * multiplier + startPos.y
+    )
+  }
   if (easeIn) {
     element.style.transition = 'ease ' + time + 's'
   } else {
     element.style.transition = 'ease-out ' + time + 's'
   }
 
+  let rotateString = ''
+
   if (!disableRotationOnAnimateOutAndBack) {
+    const rotationPower = 200
     if (getRotation(element) === 0) {
       rotateString = rotationString((Math.random() - 0.5) * rotationPower)
     } else if (getRotation(element) > 0) {
@@ -85,7 +114,11 @@ const animateOut = async (element, speed, easeIn = false, disableRotationOnAnima
 const animateBack = (element, dragTransitionDuration, disableRotationOnAnimateOutAndBack) => {
   element.style.transition = settings.snapBackDuration + 'ms'
   const startingPoint = getTranslate(element)
-  const translation = translationString(startingPoint.x * -settings.bouncePower, startingPoint.y * -settings.bouncePower)
+  const translation = translationString(
+    startingPoint.x * -settings.bouncePower,
+    startingPoint.y * -settings.bouncePower
+  )
+
   if (disableRotationOnAnimateOutAndBack) {
     element.style.transform = translation
   } else {
@@ -155,7 +188,8 @@ const dragableTouchmove = (coordinates, element, offset, lastLocation, disableDr
 }
 
 const touchCoordinatesFromEvent = (e) => {
-  const touchLocation = e.targetTouches[0]
+  const touches = e.targetTouches.length ? e.targetTouches : e.changedTouches
+  const touchLocation = touches[0]
   return { x: touchLocation.clientX, y: touchLocation.clientY }
 }
 
@@ -173,9 +207,71 @@ const getHiddenSettings = (element) => {
 
   return {
     display: 'none',
-    transform: 'translate(' + (bodySize.x + bodySize.paddingRight + elementSize.x) + 'px, 0px)',
+    transform: 'translate(' +
+      (bodySize.x + bodySize.paddingRight + elementSize.x) + 'px, 0px)',
     transition: settings.snapBackDuration + 'ms'
   }
+}
+/*
+  split screen in 3 parts (horizontally and vertically),
+  if drag ends in the side parts then return direction else return ''
+  currently not used because getSwipeDirectionByPosition offers better user experience
+
+const isSwipedByAbsolutePosition = (currentPosition) => {
+  const bodySize = getElementComputedStyle(document.body)
+
+  const deltaX = parseInt(bodySize.x / 3)
+  const deltaY = parseInt(bodySize.y/3)
+
+  if (currentPosition.x > deltaX*2) {
+    return 'right'
+  }
+  if (currentPosition.x < deltaX) {
+    return 'left'
+  }
+  if (currentPosition.y < deltaY) {
+    return 'down'
+  }
+  if (currentPosition.y > deltaY*2) {
+    return 'up'
+  }
+
+  return '';
+}
+*/
+
+/*
+  drag is considered swipe if the delta is more
+  than settings.swipeByPositionTreshold % of the screen (e.g. 25% of the screen)
+*/
+const getSwipeDirectionByPosition = (currentPosition, initialPosition) => {
+  const bodySize = getElementComputedStyle(document.body)
+
+  const tresholdX = parseInt(bodySize.x * settings.swipeByPositionTreshold * 0.01)
+  const tresholdY = parseInt(bodySize.y * settings.swipeByPositionTreshold * 0.01)
+
+  // deltaX > 0 moved left, deltaX < 0 moved right
+  const deltaX = initialPosition.x - currentPosition.x
+  // deltaY > 0 moved up, deltaX < 0 moved down
+  const deltaY = initialPosition.y - currentPosition.y
+
+  if (deltaX > tresholdX) {
+    return 'left'
+  }
+
+  if (deltaX < -tresholdX) {
+    return 'right'
+  }
+
+  if (deltaY > tresholdY) {
+    return 'up'
+  }
+
+  if (deltaY < -tresholdY) {
+    return 'down'
+  }
+
+  return ''
 }
 
 const TinderCard = React.forwardRef((
@@ -189,7 +285,9 @@ const TinderCard = React.forwardRef((
     disableDragRotation,
     dragTransitionDuration = '10ms',
     hidden,
-    disableRotationOnAnimateOutAndBack
+    disableRotationOnAnimateOutAndBack,
+    swipeBySpeed = true,
+    swipeByPosition
   },
   ref
 ) => {
@@ -203,20 +301,44 @@ const TinderCard = React.forwardRef((
       const power = 1000
       const disturbance = (Math.random() - 0.5) * 100
       if (dir === 'right') {
-        await animateOut(element.current, { x: power, y: disturbance }, true, disableRotationOnAnimateOutAndBack)
+        await animateOut(
+          element.current,
+          swipeBySpeed ? { x: power, y: disturbance } : false,
+          true,
+          disableRotationOnAnimateOutAndBack
+        )
       } else if (dir === 'left') {
-        await animateOut(element.current, { x: -power, y: disturbance }, true, disableRotationOnAnimateOutAndBack)
+        await animateOut(
+          element.current,
+          swipeBySpeed ? { x: -power, y: disturbance } : false,
+          true,
+          disableRotationOnAnimateOutAndBack
+        )
       } else if (dir === 'up') {
-        await animateOut(element.current, { x: disturbance, y: power }, true, disableRotationOnAnimateOutAndBack)
+        await animateOut(
+          element.current,
+          swipeBySpeed ? { x: disturbance, y: power } : false,
+          true,
+          disableRotationOnAnimateOutAndBack
+        )
       } else if (dir === 'down') {
-        await animateOut(element.current, { x: disturbance, y: -power }, true, disableRotationOnAnimateOutAndBack)
+        await animateOut(
+          element.current,
+          swipeBySpeed ? { x: disturbance, y: -power } : false,
+          true,
+          disableRotationOnAnimateOutAndBack
+        )
       }
       element.current.style.display = 'none'
       if (onCardLeftScreen) onCardLeftScreen(dir)
     },
     restoreCard () {
       element.current.style.display = 'block'
-      animateBack(element.current, dragTransitionDuration, disableRotationOnAnimateOutAndBack)
+      animateBack(
+        element.current,
+        dragTransitionDuration,
+        disableRotationOnAnimateOutAndBack
+      )
     },
     hideCard () {
       const hiddenSettings = getHiddenSettings()
@@ -226,19 +348,44 @@ const TinderCard = React.forwardRef((
     }
   }))
 
-  const handleSwipeReleased = React.useCallback(async (element, speed) => {
+  const handleSwipeReleased = React.useCallback(async (
+    element,
+    speed,
+    currentPosition,
+    initialPosition
+  ) => {
     if (swipeAlreadyReleased.current) { return }
     swipeAlreadyReleased.current = true
 
-    // Check if this is a swipe
-    if (Math.abs(speed.x) > settings.swipeThreshold || Math.abs(speed.y) > settings.swipeThreshold) {
-      const dir = getSwipeDirection(speed)
+    // Check if this is a swipe by speed
+    const swipedBySpeed =
+      swipeBySpeed && (
+        Math.abs(speed.x) > settings.swipeThreshold ||
+        Math.abs(speed.y) > settings.swipeThreshold
+      )
+
+    // Check if this is a swipe by position
+    let positionSwipeDir = ''
+    if (swipeByPosition) {
+      positionSwipeDir = getSwipeDirectionByPosition(currentPosition, initialPosition)
+    }
+    const swipedByPosition = swipeByPosition && !!positionSwipeDir
+
+    if (swipedBySpeed || swipedByPosition) {
+      // maybe swipeBySpeed and swipeByPosition are both enabled, prefer swipeBySpeed
+      const dir = swipedBySpeed ? getSwipeDirection(speed) : positionSwipeDir
 
       if (onSwipe) onSwipe(dir)
 
       if (flickOnSwipe) {
         if (!preventSwipe.includes(dir)) {
-          await animateOut(element, speed, undefined, disableRotationOnAnimateOutAndBack)
+          await animateOut(
+            element,
+            swipedBySpeed ? speed : false, // prefer swipeBySpeed
+            undefined,
+            disableRotationOnAnimateOutAndBack,
+            dir
+          )
           element.style.display = 'none'
           if (onCardLeftScreen) onCardLeftScreen(dir)
           return
@@ -258,12 +405,17 @@ const TinderCard = React.forwardRef((
     let offset = { x: null, y: null }
     let speed = { x: 0, y: 0 }
     let lastLocation = { x: 0, y: 0, time: new Date().getTime() }
+    let initialPosition = { x: 0, y: 0, time: new Date().getTime() }
     let mouseIsClicked = false
 
     element.current.addEventListener(('touchstart'), (ev) => {
       ev.preventDefault()
       handleSwipeStart()
       offset = { x: -touchCoordinatesFromEvent(ev).x, y: -touchCoordinatesFromEvent(ev).y }
+      if (swipeByPosition) {
+        // calc initialPosition only if swipeByPosition=true
+        initialPosition = touchCoordinatesFromEvent(ev)
+      }
     })
 
     element.current.addEventListener(('mousedown'), (ev) => {
@@ -271,6 +423,10 @@ const TinderCard = React.forwardRef((
       mouseIsClicked = true
       handleSwipeStart()
       offset = { x: -mouseCoordinatesFromEvent(ev).x, y: -mouseCoordinatesFromEvent(ev).y }
+      if (swipeByPosition) {
+        // calc initialPosition only if swipeByPosition=true
+        initialPosition = mouseCoordinatesFromEvent(ev)
+      }
     })
 
     element.current.addEventListener(('touchmove'), (ev) => {
@@ -303,16 +459,30 @@ const TinderCard = React.forwardRef((
 
     element.current.addEventListener(('touchend'), (ev) => {
       ev.preventDefault()
-      handleSwipeReleased(element.current, speed)
-      speed = { x: 0, y: 0 }
+      handleSwipeReleased(
+        element.current,
+        speed,
+        touchCoordinatesFromEvent(ev),
+        initialPosition
+      )
+      if (swipeBySpeed) {
+        speed = { x: 0, y: 0 }
+      }
     })
 
     element.current.addEventListener(('mouseup'), (ev) => {
       if (mouseIsClicked) {
         ev.preventDefault()
         mouseIsClicked = false
-        handleSwipeReleased(element.current, speed)
-        speed = { x: 0, y: 0 }
+        handleSwipeReleased(
+          element.current,
+          speed,
+          mouseCoordinatesFromEvent(ev),
+          initialPosition
+        )
+        if (swipeBySpeed) {
+          speed = { x: 0, y: 0 }
+        }
       }
     })
 
@@ -320,8 +490,15 @@ const TinderCard = React.forwardRef((
       if (mouseIsClicked) {
         ev.preventDefault()
         mouseIsClicked = false
-        handleSwipeReleased(element.current, speed)
-        speed = { x: 0, y: 0 }
+        handleSwipeReleased(
+          element.current,
+          speed,
+          mouseCoordinatesFromEvent(ev),
+          initialPosition
+        )
+        if (swipeBySpeed) {
+          speed = { x: 0, y: 0 }
+        }
       }
     })
 
